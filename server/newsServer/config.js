dc6c28b83d60733af12af9703f540389b5c9b47b
bhargav-01 <54116594+bhargav-01@users.noreply.github.com>
2021-10-20 20:03:52 +0530

    change MongoUri

--- server/newsServer/config.js
+++ server/newsServer/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : "mongodb+srv://Bhargav:Bhargav(987)@cluster0.g4von.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    'secretKey': 'Your secreat Key plz',
+    'mongoUrl' : "Your MOngoURL plz"
 }
